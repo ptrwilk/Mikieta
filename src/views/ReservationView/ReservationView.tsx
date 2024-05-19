5c4c22adc51560f40ba56927e3c47188804c8cfd
ptrwilk <90389270+ptrwilk@users.noreply.github.com>
2024-05-19 16:16:24 +0200

    MIK-83 show thanks after reservation was sent

--- src/views/ReservationView/ReservationView.tsx
+++ src/views/ReservationView/ReservationView.tsx
@@ -7,13 +7,17 @@ import {
 } from "@/components";
 import styles from "./ReservationView.module.css";
 import { useDateTimePicker, useInput, useTextArea } from "@/hooks";
-import { SubHeader } from "@/components/SubHeader/SubHeader";
 import { validateEmail, validatePhone } from "@/hooks/types";
 import { post } from "@/apihelper";
 import { ReservationRequestModel } from "@/types";
+import classNames from "classnames";
+import { useState } from "react";
+import { SubHeader } from "@/components/SubHeader/SubHeader";
 
 const ReservationView = () => {
   const REQUIRED_VALUE = "wartość wymagana";
+
+  const [thanksVisible, setThanksVisible] = useState(false);
 
   const reservation = useDateTimePicker(
     [
@@ -72,35 +76,58 @@ const ReservationView = () => {
         phone: phone.value,
         comments: comments.value,
       } as ReservationRequestModel);
+
+      setThanksVisible(true);
     }
   };
 
   return (
-    <Section className={styles["ReservationView"]}>
-      <SubHeader
-        title="ZAREZERWUJ STOLIK JUŻ TERAZ!"
-        description="Wyślij do nas swoją rezerwację, potwierdzimy ją w ciągu 24h"
-      />
-      <p className="self-start mt-12">
-        Potrzebujesz stolik na dzisiaj? Zadzwoń:{" "}
-        <span className="font-bold">333 111 222</span>
-      </p>
-      <div className="flex flex-col gap-8 w-full mt-4">
-        <DateTimePicker caption="Zarezerwuj stolik na" {...reservation} />
-        <TextInput
-          caption="Liczba osób"
-          captionTop
-          {...numberOfPeople}
-          numeric
-        />
-        <TextInput caption="Telefon" captionTop {...phone} />
-        <TextInput caption="E-mail" captionTop {...email} />
-        <TextInput caption="Imię i nazwisko" captionTop {...name} />
-        <TextArea caption="Uwagi..." rows={3} {...comments} />
-        <Button className="self-start" huge onClick={handleConfirm}>
-          Wyślij rezerwację
-        </Button>
-      </div>
+    <Section
+      className={classNames(
+        { [styles["ReservationView"]]: !thanksVisible },
+        { [styles["ReservationView-Thanks"]]: thanksVisible }
+      )}
+    >
+      {thanksVisible ? (
+        <>
+          <h2 className="text-center">Dziekujemy za złożenie rezerwacji!</h2>
+          <div className="max-w-[600px] mt-4">
+            <p>
+              Otrzymasz od nas informację zwrotną telefonicznie lub e-mailem w
+              ciągu 24 godzin. Jeśli po tym czasie nie otrzymasz żadnej
+              wiadomości, prosimy o kontakt pod numerem 333 111 222 lub adresem
+              e-mail siema@nara.com.
+            </p>
+          </div>
+        </>
+      ) : (
+        <>
+          <SubHeader
+            title="ZAREZERWUJ STOLIK JUŻ TERAZ!"
+            description="Wyślij do nas swoją rezerwację, potwierdzimy ją w ciągu 24h"
+          />
+          <p className="self-start mt-12">
+            Potrzebujesz stolik na dzisiaj? Zadzwoń:{" "}
+            <span className="font-bold">333 111 222</span>
+          </p>
+          <div className="flex flex-col gap-8 w-full mt-4">
+            <DateTimePicker caption="Zarezerwuj stolik na" {...reservation} />
+            <TextInput
+              caption="Liczba osób"
+              captionTop
+              {...numberOfPeople}
+              numeric
+            />
+            <TextInput caption="Telefon" captionTop {...phone} />
+            <TextInput caption="E-mail" captionTop {...email} />
+            <TextInput caption="Imię i nazwisko" captionTop {...name} />
+            <TextArea caption="Uwagi..." rows={3} {...comments} />
+            <Button className="self-start" huge onClick={handleConfirm}>
+              Wyślij rezerwację
+            </Button>
+          </div>
+        </>
+      )}
     </Section>
   );
 };
