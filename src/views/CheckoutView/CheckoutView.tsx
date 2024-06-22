3328c8e7a50c06957571bd0270dd664521b5d390
ptrwilk <90389270+ptrwilk@users.noreply.github.com>
2024-06-22 03:32:29 +0200

    MIK-176 fix the issue with not saving Sposób Realizacji options

--- src/views/CheckoutView/CheckoutView.tsx
+++ src/views/CheckoutView/CheckoutView.tsx
@@ -141,17 +141,15 @@ const CheckoutView = () => {
         setMessage(undefined);
         setDeliveryPrice(0);
       }
-      updateApp("order", {
-        ...app!.order,
-        deliveryMethod: value as any,
-      });
 
       const hours = getHours(value as DeliveryMethod);
       var containsHours = hours.some((x) => x.value === app!.order.openingHour);
 
-      if (!containsHours) {
-        openingHours.setValue(undefined);
-      }
+      updateApp("order", {
+        ...app!.order,
+        deliveryMethod: value as any,
+        openingHour: !containsHours ? undefined : app!.order.openingHour,
+      });
     }
   );
 
