360d7942179176fe12f1ea7384ed998e1ef317cd
RobertWilk1 <79417170+RobertWilk1@users.noreply.github.com>
2024-07-04 17:09:14 +0200

    dev fix build

--- src/components/ui/EmblaCarousel/EmblaCarousel.tsx
+++ src/components/ui/EmblaCarousel/EmblaCarousel.tsx
@@ -1,59 +1,58 @@
-import React from "react";
-import { EmblaOptionsType } from "embla-carousel";
+import React from 'react';
 import {
-  PrevButton,
-  NextButton,
-  usePrevNextButtons,
-} from "./EmblaCarouselArrowButtons";
+	PrevButton,
+	NextButton,
+	usePrevNextButtons,
+} from './EmblaCarouselArrowButtons';
 import {
-  SelectedSnapDisplay,
-  useSelectedSnapDisplay,
-} from "./EmblaCarouselSelectedSnapDisplay";
-import useEmblaCarousel from "embla-carousel-react";
-import "./embla.css";
+	SelectedSnapDisplay,
+	useSelectedSnapDisplay,
+} from './EmblaCarouselSelectedSnapDisplay';
+import useEmblaCarousel from 'embla-carousel-react';
+import './embla.css';
 
 type PropType = {
-  caption: string;
-  items: any[];
-  render: (item: any) => any;
+	caption: string;
+	items: any[];
+	render: (item: any) => any;
 };
 
 const EmblaCarousel: React.FC<PropType> = (props) => {
-  const { caption, items, render } = props;
-  const [emblaRef, emblaApi] = useEmblaCarousel({ dragFree: true });
+	const { caption, items, render } = props;
+	const [emblaRef, emblaApi] = useEmblaCarousel({ dragFree: true });
 
-  const {
-    prevBtnDisabled,
-    nextBtnDisabled,
-    onPrevButtonClick,
-    onNextButtonClick,
-  } = usePrevNextButtons(emblaApi);
+	const {
+		prevBtnDisabled,
+		nextBtnDisabled,
+		onPrevButtonClick,
+		onNextButtonClick,
+	} = usePrevNextButtons(emblaApi);
 
-  const { selectedSnap, snapCount } = useSelectedSnapDisplay(emblaApi);
+	const { selectedSnap, snapCount } = useSelectedSnapDisplay(emblaApi);
 
-  return (
-    <section className="embla">
-      <div className="embla__controls">
-        <p className="font-semibold">{caption}</p>
-        <div className="embla__buttons">
-          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
-        </div>
-      </div>
+	return (
+		<section className='embla'>
+			<div className='embla__controls'>
+				<p className='font-semibold'>{caption}</p>
+				<div className='embla__buttons'>
+					<PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+					<NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+				</div>
+			</div>
 
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {items.map((item, index) => (
-            <div className="embla__slide" key={index}>
-              {render(item)}
-            </div>
-          ))}
-        </div>
-      </div>
+			<div className='embla__viewport' ref={emblaRef}>
+				<div className='embla__container'>
+					{items.map((item, index) => (
+						<div className='embla__slide' key={index}>
+							{render(item)}
+						</div>
+					))}
+				</div>
+			</div>
 
-      <SelectedSnapDisplay selectedSnap={selectedSnap} snapCount={snapCount} />
-    </section>
-  );
+			<SelectedSnapDisplay selectedSnap={selectedSnap} snapCount={snapCount} />
+		</section>
+	);
 };
 
 export default EmblaCarousel;
