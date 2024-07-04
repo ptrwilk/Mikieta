8d6cb233c56eb5e10b369a2f32b34bc4476a32d8
RobertWilk1 <79417170+RobertWilk1@users.noreply.github.com>
2024-07-04 16:59:20 +0200

    MIK-192 Fix wrong numbers displayed in the ButtonShop

--- src/components/Button/ButtonShop.tsx
+++ src/components/Button/ButtonShop.tsx
@@ -1,38 +1,40 @@
-import classNames from "classnames";
-import { Button } from "..";
-import styles from "./ButtonShop.module.css";
-import { FaShoppingCart } from "react-icons/fa";
+import classNames from 'classnames';
+import { Button } from '..';
+import styles from './ButtonShop.module.css';
+import { FaShoppingCart } from 'react-icons/fa';
 
 interface IButtonShopProps {
-  className?: string;
-  amount?: number;
-  price?: number;
-  onClick?: () => void;
+	className?: string;
+	amount?: number;
+	price?: number;
+	onClick?: () => void;
 }
 
 const ButtonShop: React.FC<IButtonShopProps> = ({
-  className,
-  amount = 0,
-  price,
-  onClick,
+	className,
+	amount = 0,
+	price,
+	onClick,
 }) => {
-  return (
-    <Button
-      huge
-      className={classNames(className, styles["ButtonShop"])}
-      onClick={onClick}
-    >
-      <div className={styles["Content"]}>
-        <FaShoppingCart size={14} />
-        <p>{price?.toFixed(2)} zł</p>
-        {amount !== 0 && (
-          <div className={styles["Circle"]}>
-            <p className={styles["Amount"]}>{amount}</p>
-          </div>
-        )}
-      </div>
-    </Button>
-  );
+	return (
+		<Button
+			huge
+			className={classNames(className, styles['ButtonShop'])}
+			onClick={onClick}
+		>
+			<div className={styles['Content']}>
+				<FaShoppingCart size={14} />
+				<p>{price?.toFixed(2)} zł</p>
+				{amount !== 0 && (
+					<div className={styles['Circle']}>
+						<p className={classNames('text-[14px] mt-[1px]', styles['Amount'])}>
+							{amount}
+						</p>
+					</div>
+				)}
+			</div>
+		</Button>
+	);
 };
 
 export { ButtonShop };
