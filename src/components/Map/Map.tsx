d4152513fb8bb37a55253d107673a4674e5e0271
ptrwilk <90389270+ptrwilk@users.noreply.github.com>
2024-07-08 18:24:52 +0200

    MIK-204 replaced google map location

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -9,7 +9,7 @@ const Map: React.FC<IMapProps> = ({ className }) => {
   return (
     <div className={classNames(className, styles["Map"])}>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d81734.12890023208!2d18.97296089784045!3d50.19495078282119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716ce2336a1ccd1%3A0xb9af2a350559fabb!2sKatowice!5e0!3m2!1sen!2spl!4v1713868192952!5m2!1sen!2spl"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2556.990889936145!2d18.620610077436414!3d50.1426022715351!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4711498547261d35%3A0xc43296cb183aca7d!2sKsi%C4%99dza%20Adolfa%20Pojdy%205%2C%2044-238%20Czerwionka-Leszczyny!5e0!3m2!1sen!2spl!4v1720455795645!5m2!1sen!2spl"
         height="450"
         loading="lazy"
       ></iframe>
